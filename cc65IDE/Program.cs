2c8ae966084717dbaa616476860af0d700b56aad
Andrew Ross <aross69@gmail.com>
2019-10-15 23:25:16 +0100

    Add project files.

---
+++ cc65IDE/Program.cs
@@ -0,0 +1,27 @@
+﻿using cc65Wrapper;
+using System.Collections.Generic;
+
+namespace cc65IDE
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var inputFiles = new List<string>
+            {
+                "inflate.c"
+            };
+
+            var project = new CC65Project
+            {
+                TargetPlatform = "pet",
+                OptimiseCode = true,
+                WorkingDirectory = @"D:\commodore stuff\Pet Stuff\csource\inflate",
+                InputFiles = inputFiles,
+                OutputFile = "inflate"                
+            };
+            
+            var result = await Cc65Wrapper.Compile(project);
+        }
+    }
+}
---
+++ cc65Wrapper/CC65Project.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace cc65Wrapper
+{
+    public class CC65Project
+    {
+        const int VERSION = 1000;
+
+        public string WorkingDirectory { get; set; }
+        public string TargetPlatform { get; set; }
+        public List<string> InputFiles { get; set; }
+        public string OutputFile { get; set; }
+        public bool OptimiseCode { get; set; }
+
+        public int Version { get; set; }
+
+        public CC65Project()
+        {
+            Version = VERSION;
+        }
+
+        public string AsJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
