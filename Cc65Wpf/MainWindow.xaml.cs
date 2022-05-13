718a2d40047c6a10df0c067c74e32595b170326b
Andrew Ross <aross69@gmail.com>
2022-05-13 19:47:05 +0100

    + Have a basic WPF app using the AvalonEdit syntax highlighting control

---
+++ Cc65Wpf/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Xml;
+
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Search;
+using Microsoft.Win32;
+
+namespace Cc65Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+		string currentFileName;
+
+		void openFileClick(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.CheckFileExists = true;
+			if (dlg.ShowDialog() ?? false)
+			{
+				currentFileName = dlg.FileName;
+				textEditor.Load(currentFileName);
+				textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(currentFileName));
+			}
+		}
+
+		void saveFileClick(object sender, EventArgs e)
+		{
+			if (currentFileName == null)
+			{
+				SaveFileDialog dlg = new SaveFileDialog();
+				dlg.DefaultExt = ".txt";
+				if (dlg.ShowDialog() ?? false)
+				{
+					currentFileName = dlg.FileName;
+				}
+				else
+				{
+					return;
+				}
+			}
+			textEditor.Save(currentFileName);
+		}
+
+		#region Folding
+		FoldingManager foldingManager;
+		object foldingStrategy;
+
+		void HighlightingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (textEditor.SyntaxHighlighting == null)
+			{
+				foldingStrategy = null;
+			}
+			else
+			{
+				switch (textEditor.SyntaxHighlighting.Name)
+				{
+					case "XML":
+						foldingStrategy = new XmlFoldingStrategy();
+						textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+						break;
+					case "C#":
+					case "C++":
+					case "PHP":
+					case "Java":
+						textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.CSharp.CSharpIndentationStrategy(textEditor.Options);
+						foldingStrategy = new BraceFoldingStrategy();
+						break;
+					default:
+						textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+						foldingStrategy = null;
+						break;
+				}
+			}
+			if (foldingStrategy != null)
+			{
+				if (foldingManager == null)
+					foldingManager = FoldingManager.Install(textEditor.TextArea);
+				UpdateFoldings();
+			}
+			else
+			{
+				if (foldingManager != null)
+				{
+					FoldingManager.Uninstall(foldingManager);
+					foldingManager = null;
+				}
+			}
+		}
+
+		void UpdateFoldings()
+		{
+			if (foldingStrategy is BraceFoldingStrategy)
+			{
+				((BraceFoldingStrategy)foldingStrategy).UpdateFoldings(foldingManager, textEditor.Document);
+			}
+			if (foldingStrategy is XmlFoldingStrategy)
+			{
+				((XmlFoldingStrategy)foldingStrategy).UpdateFoldings(foldingManager, textEditor.Document);
+			}
+		}
+		#endregion
+	}
+}
